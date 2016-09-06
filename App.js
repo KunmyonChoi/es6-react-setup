6a22e220c9817395b48e5f0b29d669fe203847ef
KunmyonChoi <dilly97@gmail.com>
2016-09-07 01:07:09 +0900

    Multiple element in return

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-// class App extends React.Component {
-// 	render() {
-// 		return React.createElement('h1', null, 'Hello Guys');
-// 	}
-// }
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World</h1>
+				<h1>Hi there</h1>
+			</div>
+			);
+	}
+}
 
-const App = () => <h1>Hello Stateless function</h1>;
+// const App = () => <h1>Hello Stateless function</h1>;
 
 export default App
