2f17957e63e17cadd17c043e7d6a902d42dcbc9d
KunmyonChoi <dilly97@gmail.com>
2016-09-07 01:17:09 +0900

    props and propTypes

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-ReactDOM.render(<App />, document.getElementById('app'));
+// import App from "./App";
+
+class App extends React.Component {
+	render () {
+		let txt = this.props.txt;
+		return <h1>Hello, {txt}</h1>
+	}
+}
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+};
+
+ReactDOM.render(
+	<App txt="this is the props value." />, 
+	document.getElementById('app')
+);
